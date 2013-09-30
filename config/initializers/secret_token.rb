e5650e65931edc24de4002bcfe1a787b6acbe43d
kienbt <bui.trung.kien@framgia.com>
2013-09-30 10:24:33 +0700

    initial project commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+J2bc::Application.config.secret_key_base = '175a27b3b1bfcd939ddd5405b402550aa9edda05c62078722de8692e9d0eccb255e782d50fbf3e792cbeb2a01b37ed8d3b2bc0b19d97b3538ab6bb7d37fe6b7e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+J2bc::Application.config.session_store :cookie_store, key: '_j2bc_session'
