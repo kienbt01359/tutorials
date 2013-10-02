d67b1242e9f7c740c11ba9448be56dcf98443b96
kienbt <bui.trung.kien@framgia.com>
2013-10-02 11:44:44 +0700

    Chapter 8: Sign in and Sign out

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,16 @@ J2bc::Application.routes.draw do
   resources :microposts
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
  # The priority is based upon order of creation: first created -> highest priority.
   root  'static_pages#home'
-  match '/help',    to: 'static_pages#help',    via: 'get'
-  match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
-  match '/signup', to: 'users#new', via: 'get'
+  match '/help',    to: 'static_pages#help',    via: :get
+  match '/about',   to: 'static_pages#about',   via: :get
+  match '/contact', to: 'static_pages#contact', via: :get
+  match '/signup', to: 'users#new', via: :get
+  match '/signin', to: 'sessions#new', via: :get
+  match '/signout', to: 'sessions#destroy', via: :delete
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
